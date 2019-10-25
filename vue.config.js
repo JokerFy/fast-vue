8e67351a6cb26c9e76a6772321d37ca79577b0e5
daxiongYang <daxiong.yang@qq.com>
2019-10-25 17:59:55 +0800

    升级到vue-cli 3

---
+++ vue.config.js
@@ -0,0 +1,31 @@
+/**
+ * 配置参考: https://cli.vuejs.org/zh/config/
+ */
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  lintOnSave: false,
+  configureWebpack: config => {
+    config.externals = {
+      mockjs: 'Mock',
+      echarts: 'echarts',
+      ueditor: 'UE'
+    }
+  },
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+  },
+  productionSourceMap: false,
+  devServer: {
+    open: true,
+    port: 8001,
+    overlay: {
+      errors: true,
+      warnings: true
+    }
+  }
+}
